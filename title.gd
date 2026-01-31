13202c61e42b1b589cdb8d681207ed939fabc29a
jebkam <jebkam@gmail.com>
2026-01-31 15:58:02 -0800

    [art] add basic placeholders. [game] create base scene with followers and totem. [title] create basic title menu that goes to game. [project] update start scene to title.

---
+++ title.gd
@@ -0,0 +1,9 @@
+extends Control
+@onready var bttn_start: Button = $MarginContainer/VBoxContainer/VBoxContainer/Bttn_Start
+@onready var bttn_credits: Button = $MarginContainer/VBoxContainer/VBoxContainer/Bttn_Credits
+
+func _ready():
+	bttn_start.pressed.connect(Pressed_Start)
+	
+func Pressed_Start():
+	get_tree().change_scene_to_file("res://village.tscn")
